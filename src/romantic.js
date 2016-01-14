2b89ebf8302f8666428a61371999923d77618fcd
EGOIST <0x142857@gmail.com>
2016-01-14 23:23:36 +0800

    fix lint

--- src/romantic.js
+++ src/romantic.js
@@ -2,17 +2,16 @@ export default class Romantic {
 	constructor(regex = /\<([a-zA-Z0-9_-]+)\>/g) {
 		this.regex = regex
 	}
-	
 	match(romanticString, input) {
-	  romanticString = romanticString.split(/\s+/g)
-	  input = input.split(/\s+/g)
-	  const result = {}
-	  romanticString.forEach((roman, i) => {
-	    if (this.regex.test(roman)) {
-	      const key = roman.replace(this.regex, '$1')
-	      result[key] = input[i]
-	    }
-	  })
-	  return result
+		romanticString = romanticString.split(/\s+/g)
+		input = input.split(/\s+/g)
+		const result = {}
+		romanticString.forEach((roman, i) => {
+			if (this.regex.test(roman)) {
+				const key = roman.replace(this.regex, '$1')
+				result[key] = input[i]
+			}
+		})
+		return result
 	}
 }
